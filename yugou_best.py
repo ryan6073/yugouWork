b253e3bd72820de84b0bd0aba873ffa1c39ac944
xiaochaoroulalala <1206174039@qq.com>
2023-11-24 18:23:34 +0800

    1.4翟

--- yugou_best.py
+++ yugou_best.py
@@ -1,42 +1,53 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeClassifier
+
 plt.rcParams["font.sans-serif"] = "SimHei"  # 解决中文乱码问题
 # 清理内存
 import gc
 import seaborn as sns
 import random
 
-df_train = pd.read_csv(r'data_format1/train_format1.csv')
-df_test = pd.read_csv(r'data_format1/test_format1.csv')
-user_info = pd.read_csv(r'data_format1/user_info_format1.csv')
-user_log = pd.read_csv(r'data_format1/user_log_format1.csv')
+# # from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.metrics import accuracy_score
+# from sklearn import model_selection
+# from sklearn.neighbors import KNeighborsRegressor
 
-# 检查缺失值
-user_info['gender'].replace(2.0, np.nan, inplace=True)
-missing_values = user_info.isnull().sum()
-print("缺失值统计：")
-print(missing_values)
+df_train = pd.read_csv(r'data/data_format1/train_format1.csv')
+df_test = pd.read_csv(r'data/data_format1/test_format1.csv')
+user_info = pd.read_csv(r'data/data_format1/user_info_format1.csv')
+user_log = pd.read_csv(r'data/data_format1/user_log_format1.csv')
 
-missing_values = user_log.isnull().sum()
-print(missing_values)
+print(df_test.shape, df_train.shape)
+print(user_info.shape, user_log.shape)
 
-user_log = user_log.fillna(method='ffill')
-
-# 缺失值处理:使用均值填充
-user_info['gender'].replace(2.0, np.nan, inplace=True)
+# 填充缺失值
 user_info['age_range'].replace(0.0, np.nan, inplace=True)
-user_info.fillna(user_info.mean(), inplace=True)
+user_info['gender'].replace(2.0, np.nan, inplace=True)
+user_info['age_range'].replace(np.nan, -1, inplace=True)
+user_info['gender'].replace(np.nan, -1, inplace=True)
 
 # 聚合特征
 seller_group = user_log.groupby(["seller_id", "action_type"]).count()[["user_id"]].reset_index().rename(
     columns={'user_id': 'count'})
 
+# del user_log
+# gc.collect()
+
 # age_range,gender特征添加
 df_train = pd.merge(df_train, user_info, on="user_id", how="left")
+df_train.head()
 
 total_logs_temp = user_log.groupby([user_log["user_id"], user_log["seller_id"]]).count().reset_index()[
     ["user_id", "seller_id", "item_id"]]
@@ -89,12 +100,6 @@ four_features = one_clicks_temp.groupby(
 four_features = four_features.drop(["action_type", "times"], axis=1)
 df_train = pd.merge(df_train, four_features, on=["user_id", "merchant_id"], how="left")
 
-# 检查缺失值
-missing_values = df_train.isnull().sum()
-
-print("缺失值统计：")
-print(missing_values)
-
 # 缺失值处理
 df_train.isnull().sum(axis=0)
 df_train = df_train.fillna(method='ffill')
@@ -102,79 +107,52 @@ df_train = df_train.fillna(method='ffill')
 # 模型构建与调参
 Y = df_train['label']
 X = df_train.drop(['user_id', 'merchant_id', 'label'], axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=10)
+# df_train = pd.merge(df_train, browse_days_temp1, on=["user_id", "merchant_id"], how="left")
 
-mlp = MLPClassifier(solver='lbfgs', activation='relu', alpha=0.1, random_state=0, hidden_layer_sizes=[10, 10]).fit(
-    X_train, y_train)
-Predict = mlp.predict(X_test)
-Predict_proba = mlp.predict_proba(X_test)
-print(Predict_proba[:])
-Score = accuracy_score(y_test, Predict)
-print(Score)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.55, random_state=10)
 
-#决策树
-from sklearn.tree import DecisionTreeClassifier
-tree = DecisionTreeClassifier(max_depth=5,random_state=0)
-tree.fit(X_train, y_train)
-Predict_proba = tree.predict_proba(X_test)
-print(Predict_proba[:])
-print("Accuracy on training set: {:.8f}".format(tree.score(X_train, y_train)))
-print("Accuracy on test set: {:.8f}".format(tree.score(X_test, y_test)))
 
-from sklearn.ensemble import RandomForestClassifier
-forest = RandomForestClassifier(n_estimators=10, random_state=2)
-forest.fit(X_train, y_train)
-Predict_proba = forest.predict_proba(X_test)
-print(Predict_proba[:])
-print("Accuracy on training set: {:.8f}".format(forest.score(X_train, y_train)))
-print("Accuracy on test set: {:.8f}".format(forest.score(X_test, y_test)))
+# 初始化梯度提升树分类器
+gradient_boosting = GradientBoostingClassifier(n_estimators=100, random_state=42)  # 这里 n_estimators 表示基学习器的数量
 
-from sklearn.ensemble import GradientBoostingClassifier
-gbrt = GradientBoostingClassifier(random_state=0)
-gbrt.fit(X_train, y_train)
-Predict_proba = gbrt.predict_proba(X_test)
-print(Predict_proba[:])
-print("Accuracy on training set: {:.8f}".format(gbrt.score(X_train, y_train)))
-print("Accuracy on test set: {:.8f}".format(gbrt.score(X_test, y_test)))
+# 训练梯度提升树模型
+gradient_boosting.fit(X_train, y_train)
 
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+# 特征选择
+select_features = SelectFromModel(gradient_boosting, threshold='median')  # 这里使用梯度提升树模型进行特征选择
+select_features.fit(X_train, y_train)
 
-# 创建一个决策树基分类器
-base_classifier = DecisionTreeClassifier(max_depth=1)
+# 提取重要特征
+X_train_selected = select_features.transform(X_train)
+X_test_selected = select_features.transform(X_test)
 
-# 创建 AdaBoost 分类器
-adaboost = AdaBoostClassifier(base_estimator=base_classifier, n_estimators=50, learning_rate=0.1, random_state=0)
+# 使用选择的重要特征重新训练模型
+gradient_boosting_selected = GradientBoostingClassifier(n_estimators=100, random_state=42)
+gradient_boosting_selected.fit(X_train_selected, y_train)
 
-adaboost.fit(X_train, y_train)
+# 定义参数网格
+param_grid = {
+    'n_estimators': [15,25,50],  # 调整基学习器的数量
+    'learning_rate': [0.003,0.005,0.01],  # 学习率
+    'max_depth': [1,3, 5]  # 调整树的深度
+    # 其他需要调整的参数
+}
 
-predict_proba = adaboost.predict_proba(X_test)
-print(predict_proba[:])
+# 使用 GridSearchCV 进行参数调优
+grid_search = GridSearchCV(estimator=gradient_boosting_selected, param_grid=param_grid, cv=3, scoring='accuracy')
+grid_search.fit(X_train_selected, y_train)
 
-print("Accuracy on training set: {:.8f}".format(adaboost.score(X_train, y_train)))
-print("Accuracy on test set: {:.8f}".format(adaboost.score(X_test, y_test)))
+# 输出最佳参数组合和对应的准确率
+print("Best Parameters:", grid_search.best_params_)
 
-# 网格搜索寻找最优参数
 
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import GridSearchCV
+# 使用最佳参数组合重新训练模型
+best_gradient_boosting = grid_search.best_estimator_
+best_gradient_boosting.fit(X_train_selected, y_train)
 
-# 创建一个决策树基分类器
-base_classifier = DecisionTreeClassifier(max_depth=1)
+# 对测试集进行预测
+predictions = best_gradient_boosting.predict(X_test_selected)
 
-# 创建 AdaBoost 分类器
-adaboost = AdaBoostClassifier(base_estimator=base_classifier, n_estimators=50, learning_rate=0.1, random_state=0)
-
-# 定义深度的候选值
-param_grid = {'base_estimator__max_depth': [1, 2, 3, 4, 5]}
-
-# 使用网格搜索
-grid_search = GridSearchCV(adaboost, param_grid, cv=5)
-grid_search.fit(X_train, y_train)
-
-# 获取最佳深度
-best_depth = grid_search.best_params_['base_estimator__max_depth']
-best_depth
+# 计算准确率
+accuracy = accuracy_score(y_test, predictions)
+print("Gradient Boosting Accuracy after Parameter Tuning:", accuracy)
